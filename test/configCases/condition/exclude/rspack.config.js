e1ba700df1360cf230a7aab68cc0031b4c02cf9f
Xu Yiming <xuyiming.open@outlook.com>
2024-09-14 01:10:47 +0800

    test: Set target to launch the right runner

--- test/configCases/condition/exclude/rspack.config.js
+++ test/configCases/condition/exclude/rspack.config.js
@@ -3,6 +3,7 @@ const ReactRefreshRspackPlugin = require('../../../..');
 /** @type {import('@rspack/core').Configuration} */
 module.exports = {
   mode: 'development',
+  target: 'web',
   context: __dirname,
   entry: './index.js',
   plugins: [new ReactRefreshRspackPlugin({
