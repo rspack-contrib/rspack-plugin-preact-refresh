c3bf57bbb1b983e454f5df664c4ce555e22ffaa9
neverland <chenjiahan.jait@bytedance.com>
2024-11-11 14:38:43 +0800

    refactor(build): switch to Rslib

---
+++ index.cjs
@@ -0,0 +1,10 @@
+/**
+ * Compatible with CommonJS
+ * ```js
+ * const { PreactRefreshRspackPlugin } = require('@rspack/plugin-preact-refresh');
+ * ```
+ */
+
+const { PreactRefreshRspackPlugin } = require('./dist/index.cjs');
+
+module.exports = PreactRefreshRspackPlugin;
